5a32d398dbb4bf12713195ad7919970f2aa7d93c
andreadev237 <andreadev237@gmail.com>
2025-04-23 21:46:35 +0200

    Fix for netlify export

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  distDir : ".out"
+  distDir : "out",
+  output: "export"
 };
 
 export default nextConfig;
